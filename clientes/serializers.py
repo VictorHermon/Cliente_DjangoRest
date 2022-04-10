32c71a1fcfc12cc209ed5dcadf258c75f6b35d7e
victorhermon <victorsilva.silva6@gmail.com>
2022-04-10 16:16:42 -0300

    Correção na verificação do nome digitado

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -11,7 +11,7 @@ class ClienteSerializer(serializers.ModelSerializer):
     def validate(self, data):
         if not cpf_valido(data['cpf']):
             raise serializers.ValidationError({'cpf': 'Número de CPF inválido'})
-        if not nome_valido(data['nome']):
+        if nome_valido(data['nome']):
             raise serializers.ValidationError({'nome': 'Não inclua números neste campo'})
         if not rg_valido(data['rg']):
             raise serializers.ValidationError({'rg': 'O RG deve ter 9 dígitos'})
--- clientes/validators.py
+++ clientes/validators.py
@@ -3,20 +3,25 @@ from validate_docbr import CPF
 
 
 def cpf_valido(numero_cpf):
+    """Verifica se o CPF digitado é valido"""
     cpf = CPF()
     return cpf.validate(numero_cpf)
 
 
 def nome_valido(nome):
-    return nome.isalpha()
+    """Verifica se o nome digitado possui números"""
+    modelo_nome = '[0-9]'
+    nome_correto = re.findall(modelo_nome, nome)
+    return nome_correto
 
 
 def rg_valido(rg):
+    """Verifica se o RG digitado tem 9 digitos"""
     return len(rg) == 9
 
 
 def celular_valido(numero_celular):
-    """Verifica se o número de celular é valido (11 91234-1234)"""
+    """Verifica se o número de celular está no modelo (11 91234-1234)"""
     modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
     resposta = re.findall(modelo, numero_celular)
     return resposta
